0864de2d72fde3e106f1d3c1652a16263769e638
Souvik <souvikchandra9454@gmail.com>
2022-08-18 22:17:19 +0530

    first commit

---
+++ backend_sanity/schemas/blogs.js
@@ -0,0 +1,18 @@
+export default{
+    name:'blogs',
+    title:'Blogs',
+    type: 'document',
+    fields:[
+        {
+            name:'title',
+            title:'Title',
+            type:'string'
+        },
+        {
+            name:'description',
+            title:'Description',
+            type:'string'
+        },
+        
+    ]
+}
